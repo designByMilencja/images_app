221e723ca03016abf8a814cfb0f4588d0c81400b
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-25 22:05:27 +0200

    feat(constants): added content

--- constants/index.ts
+++ constants/index.ts
@@ -75,3 +75,18 @@ export const footerLinks = [
         ],
     },
 ];
+export const categoryFilters = [
+    "Frontend",
+    "Backend",
+    "Full-Stack",
+    "Mobile",
+    "UI/UX",
+    "Game Dev",
+    "DevOps",
+    "Data Science",
+    "Machine Learning",
+    "Cybersecurity",
+    "Blockchain",
+    "E-commerce",
+    "Chatbots"
+]
