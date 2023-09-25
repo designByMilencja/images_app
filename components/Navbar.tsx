a698355b40b31c879e667401c6a687e90459b392
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-25 15:17:57 +0200

    feat(Navbar): isolated client component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import {navLinks} from "@/constants";
 import AuthProviders from "@/components/AuthProviders";
 import {getCurrentUser} from "@/lib/session";
+import ProfileMenu from "@/components/ProfileMenu";
 
 const Navbar = async () => {
     const session = await getCurrentUser();
@@ -11,10 +12,10 @@ const Navbar = async () => {
             <div className="bg-orange-400 flex-1 flexStart gap-10">
                 <Link href="/">
                     <Image
-                    src="/icons/bug-outline.svg"
-                    width={50}
-                    height={50}
-                    alt="a logo item - the little bug outline"/>
+                        src="/icons/bug-outline.svg"
+                        width={50}
+                        height={50}
+                        alt="a logo item - the little bug outline"/>
                 </Link>
                 <ul className="bg-blue-100 xs:flex hidden text-small gap-7">
                     {navLinks.map((link) => (
@@ -25,15 +26,7 @@ const Navbar = async () => {
             <div className="bg-pink-800 flexCenter gap-4">
                 {session?.user ? (
                     <>
-                        {session?.user?.image
-                        &&   (<Image
-                                src={session.user.image}
-                                width={40}
-                                height={40}
-                                className="rounded-full"
-                                alt={session.user.name}
-                            />)}
-
+                        <ProfileMenu session={session}/>
                         <Link href='/create-project'>Share work</Link>
                     </>
                 ) : <AuthProviders/>}
