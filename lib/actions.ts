e45758307b18cebe0eb8604a88ee47da64ec0405
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-25 12:51:10 +0200

    fix(): typo

--- lib/actions.ts
+++ lib/actions.ts
@@ -7,7 +7,7 @@ const apiUrl = isProduction ?
     : 'http://127.0.0.1:4000/graphql';
 const apiKey = isProduction ?
     process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ''
-    : 'letmein';
+    : 'letMeIn';
 const serverUrl = isProduction ?
     process.env.NEXT_PUBLIC_SERVER_URL
     : 'http://localhost:3000';
