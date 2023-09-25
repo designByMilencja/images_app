7d58112fdc0645a2c85fe633b0c422702e189444
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-25 18:48:12 +0200

    feat(Modal): added modal with button to close it

---
+++ components/Modal.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import React, {ReactNode, useCallback, useRef} from "react";
+import {useRouter} from "next/navigation";
+import Image from "next/image";
+
+const Modal = ({children}: { children: ReactNode }) => {
+    const overlay = useRef<HTMLDivElement>(null);
+    const wrapper = useRef<HTMLDivElement>(null);
+    const router = useRouter();
+
+    const onDismiss = useCallback(() => {
+        router.push('/')
+    }, [router]);
+
+    const handleClick = useCallback((e: React.MouseEvent) => {
+        if ((e.target === overlay.current) && onDismiss) {
+            onDismiss()
+        }
+    }, [onDismiss, overlay]);
+
+
+    return (
+        <div ref={overlay} className="modal" onClick={handleClick}>
+            <button type="button" onClick={onDismiss} className='absolute top-1 right-8'>
+                <Image src="/icons/bug-outline.svg" width={17} height={17} alt="close" className="bg-white"/>
+            </button>
+            <div ref={wrapper} className="modal_wrapper">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
