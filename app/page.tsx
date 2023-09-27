0454126772d25303154dcb59dd542df4f6a61a5e
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-27 20:39:59 +0200

    feat(page): added home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,46 @@
-
-export default function Home() {
-  return (
-      <div>
-
-      </div>
-  )
+import {fetchAllProjects} from "@/lib/actions";
+import {ProjectInterface} from "@/common.types";
+import ProjectCard from "@/components/ProjectCard";
+type ProjectSearch = {
+    projectSearch: {
+        edges: { node: ProjectInterface }[];
+        pageInfo: {
+            hasPreviousPage: boolean;
+            hasNextPage: boolean;
+            startCursor: string;
+            endCursor: string;
+        };
+    },
 }
+const Home = async () => {
+    const data = await fetchAllProjects() as ProjectSearch;
+    const projectsToDisplay = data?.projectSearch?.edges || [];
+    if (projectsToDisplay.length === 0) {
+        return (
+            <section className="flexStart flex-col paddings">
+                Categories
+                <p className="no-result-text text-center">No projects found, go create some first</p>
+            </section>
+        )
+    }
+  return (
+      <section className="flexStart flex-col paddings mb-16">
+          <h1>Categories</h1>
+          <section className="projects-grid">
+              {projectsToDisplay.map(({node}: {node: ProjectInterface}) => (
+                  <ProjectCard
+                  key={node?.id}
+                  id={node?.id}
+                  image={node?.image}
+                  title={node?.title}
+                  name={node?.createdBy.name}
+                  avatarUrl={node?.createdBy.avatarUrl}
+                  userId={node?.createdBy.id}
+                  />
+              ))}
+          </section>
+
+      </section>
+  )
+};
+export default Home
