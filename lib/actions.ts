65a586b371c099cd81e87f736946d40c17d4f107
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-28 11:45:38 +0200

    fix(): changed type

--- lib/actions.ts
+++ lib/actions.ts
@@ -79,11 +79,9 @@ export const createNewProject = async (form: ProjectForm, creatorId: string, tok
     }
 }
 
-export const fetchAllProjects = async (category?:string, endcursor?:string) => {
-    client.setHeader('x-api-key', apiKey);
-    return makeGraphQLRequest(projectsQuery, {
-        category, endcursor
-    })
+export const fetchAllProjects = (category?: string | null, endcursor?: string | null) => {
+    client.setHeader("x-api-key", apiKey);
 
-}
+    return makeGraphQLRequest(projectsQuery, { category, endcursor });
+};
 
