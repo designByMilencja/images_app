44615d256eec2f77f578969840e0d0a36df1c028
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-02 17:38:07 +0200

    fix(): added search params

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import {fetchAllProjects} from "@/lib/actions";
-import {ProjectInterface} from "@/common.types";
-import ProjectCard from "@/components/ProjectCard";
+import { ProjectInterface } from "@/common.types";
 import Categories from "@/components/Categories";
+import ProjectCard from "@/components/ProjectCard";
+import { fetchAllProjects } from "@/lib/actions";
+
+type SearchParams = {
+    category?: string;
+    endcursor?: string | null;
+}
+
+type Props = {
+    searchParams: SearchParams;
+}
+
 type ProjectSearch = {
     projectSearch: {
         edges: { node: ProjectInterface }[];
@@ -13,14 +23,18 @@ type ProjectSearch = {
         };
     },
 }
-const Home = async () => {
-    const data = await fetchAllProjects('Frontend') as ProjectSearch;
+
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+    const data = await fetchAllProjects(category, endcursor) as ProjectSearch;
+
     const projectsToDisplay = data?.projectSearch?.edges || [];
+
     if (projectsToDisplay.length === 0) {
         return (
             <section className="flexStart flex-col paddings">
-                Categories
-                <p className="no-result-text text-center">No projects found, go create some first</p>
+                <Categories />
+
+                <p className="no-result-text text-center">No projects found, go create some first.</p>
             </section>
         )
     }
@@ -44,4 +58,5 @@ const Home = async () => {
       </section>
   )
 };
-export default Home
+
+export default Home;
